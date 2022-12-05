42918c3e3b47c8da6458b58edbe5d29d4dd65724
Sebastian Alarcon Diaz <juan.alarcon22@misena.edu.co>
2022-12-04 23:50:13 -0500

    index.php

--- index.php
+++ index.php
@@ -5,7 +5,4 @@ use Core\FrontController;
 require_once 'vendor/autoload.php';
 require_once 'Helpers/routes.php';
 
-$dotenv = Dotenv\Dotenv ::createImmutable(__DIR__);
-$dotenv -> load();
-
 (new FrontController($_GET['action'] ?? 'login')) -> run();
