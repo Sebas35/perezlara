118bb1a61147f67797bdd46a817781812ef07cb5
Sebastian Alarcon Diaz <90793992+Sebas35@users.noreply.github.com>
2022-12-05 18:29:27 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,5 @@ use Core\FrontController;
 
 require_once 'vendor/autoload.php';
 require_once 'Helpers/routes.php';
-
+print_r($_GET['action'] ?? 'n');
 (new FrontController($_GET['action'] ?? 'login')) -> run();
