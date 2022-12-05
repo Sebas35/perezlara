e1b659eadc22f4e0a8a036b36836106344a65a06
Sebastian Alarcon Diaz <90793992+Sebas35@users.noreply.github.com>
2022-12-05 14:22:09 -0500

    Update dashboard.php

--- resources/views/dashboard.php
+++ resources/views/dashboard.php
@@ -3,13 +3,13 @@
 $title = 'Dashboard';
 $css_template = false;
 $css = '<link rel="stylesheet" href="' . ('http://localhost/perezlara/resources/css/dashboard.css') . '">';
-$js = '<script src="' . js('cloud.js') . '"></script>' .
-$js = '<script src="' . js('actions/dashboard.js') . '"></script>' .
-      '<script src="' . js('input.js') . '"></script>' .
-      '<script src="' . view('components/form_elements/dropdown/filter_select.js') . '"></script>'.
-      '<script src="' . js('table.js') . '"></script>';
+$js = '<script src="' . ('http://localhost/perezlara/resources/js/cloud.js') . '"></script>' .
+$js = '<script src="' . ('http://localhost/perezlara/resources/js/actions/dashboard.js') . '"></script>' .
+      '<script src="' . ('http://localhost/perezlara/resources/js/input.js') . '"></script>' .
+      '<script src="' . ('http://localhost/perezlara/resources/view/components/form_elements/dropdown/filter_select.js') . '"></script>'.
+      '<script src="' . ('http://localhost/perezlara/resources/js/table.js') . '"></script>';
 $content = 'main_dashboard.php';
 $group = 'search.php';
 $filters = [['Seguro'], ['Aseguradora'],];
 $buttons_filters = false;
-require_once view('components/app/app.php');
+require_once ('http://localhost/perezlara/resources/view/components/app/app.php');
