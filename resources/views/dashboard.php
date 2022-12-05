7d47dcbf3f6e3296b4d358fcc5c5a79011428f09
Sebastian Alarcon Diaz <90793992+Sebas35@users.noreply.github.com>
2022-12-05 14:29:28 -0500

    Update dashboard.php

--- resources/views/dashboard.php
+++ resources/views/dashboard.php
@@ -4,7 +4,7 @@ $title = 'Dashboard';
 $css_template = false;
 $css = '<link rel="stylesheet" href="' . ('../css/dashboard.css') . '">';
 $js = '<script src="' . ('../js/cloud.js') . '"></script>' .
-$js = '<script src="' . ('../js/actions/dashboard.js') . '"></script>' .
+      '<script src="' . ('../js/actions/dashboard.js') . '"></script>' .
       '<script src="' . ('../js/input.js') . '"></script>' .
       '<script src="' . ('components/form_elements/dropdown/filter_select.js') . '"></script>'.
       '<script src="' . ('../js/table.js') . '"></script>';
@@ -12,4 +12,6 @@ $content = 'main_dashboard.php';
 $group = 'search.php';
 $filters = [['Seguro'], ['Aseguradora'],];
 $buttons_filters = false;
+echo $css;
+echo $js;
 require_once ('components/app/app.php');
