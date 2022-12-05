80c38cc0080b869d56d49215c17ee65892c20148
Sebastian Alarcon Diaz <90793992+Sebas35@users.noreply.github.com>
2022-12-05 14:49:46 -0500

    Update dashboard.php

--- resources/views/dashboard.php
+++ resources/views/dashboard.php
@@ -2,14 +2,14 @@
 
 $title = 'Dashboard';
 $css_template = false;
-$css = '<link rel="stylesheet" href="../css/dashboard.css">';
-$js = '<script src="../js/cloud.js"></script>' .
-      '<script src="../js/actions/dashboard.js"></script>' .
-      '<script src="../js/input.js"></script>' .
-      '<script src="components/form_elements/dropdown/filter_select.js"></script>'.
-      '<script src="../js/table.js"></script>';
+$css = '<link rel="stylesheet" href="' . css('dashboard.css') . '">';
+$js = '<script src="' . js('cloud.js') . '"></script>' .
+$js = '<script src="' . js('actions/dashboard.js') . '"></script>' .
+      '<script src="' . js('input.js') . '"></script>' .
+      '<script src="' . view('components/form_elements/dropdown/filter_select.js') . '"></script>'.
+      '<script src="' . js('table.js') . '"></script>';
 $content = 'main_dashboard.php';
 $group = 'search.php';
 $filters = [['Seguro'], ['Aseguradora'],];
 $buttons_filters = false;
-require_once 'components/app/app.php';
+require_once view('components/app/app.php');
