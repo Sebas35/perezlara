7e7870edda264bcbdd645b1d05db6c4a41a53e52
Sebastian Alarcon Diaz <90793992+Sebas35@users.noreply.github.com>
2022-12-05 14:33:47 -0500

    Update dashboard.php

--- resources/views/dashboard.php
+++ resources/views/dashboard.php
@@ -2,12 +2,12 @@
 
 $title = 'Dashboard';
 $css_template = false;
-$css = '<link rel="stylesheet" href="' . ('../css/dashboard.css') . '">';
-$js = '<script src="' . ('../js/cloud.js') . '"></script>' .
-      '<script src="' . ('../js/actions/dashboard.js') . '"></script>' .
-      '<script src="' . ('../js/input.js') . '"></script>' .
-      '<script src="' . ('components/form_elements/dropdown/filter_select.js') . '"></script>'.
-      '<script src="' . ('../js/table.js') . '"></script>';
+$css = '<link rel="stylesheet" href="../css/dashboard.css">';
+$js = '<script src="../js/cloud.js"></script>' .
+      '<script src="../js/actions/dashboard.js"></script>' .
+      '<script src="../js/input.js"></script>' .
+      '<script src="components/form_elements/dropdown/filter_select.js"></script>'.
+      '<script src="../js/table.js"></script>';
 $content = 'main_dashboard.php';
 $group = 'search.php';
 $filters = [['Seguro'], ['Aseguradora'],];
