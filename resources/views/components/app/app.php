fb65e56fe0b89a6c31744a502bfe1d683d655afa
Sebastian Alarcon Diaz <90793992+Sebas35@users.noreply.github.com>
2022-12-05 14:48:08 -0500

    Update app.php

--- resources/views/components/app/app.php
+++ resources/views/components/app/app.php
@@ -5,11 +5,11 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" href="<?php echo '../../icons/window/icono.svg'; ?>">
+    <link rel="icon" href="<?php echo '../icons/window/icono.svg'; ?>">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap">
-    <link rel="stylesheet" href="<?php echo '../../css/components/app/app.css'; ?>">
+    <link rel="stylesheet" href="<?php echo '../css/components/app/app.css'; ?>">
     <?php if (!isset($css_template)): ?>
-        <link rel="stylesheet" href="<?php echo '../../../css/components/app/content.css'; ?>">
+        <link rel="stylesheet" href="<?php echo '../css/components/app/content.css'; ?>">
     <?php endif; ?>
 
     <?php
